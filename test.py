30da0b3e5eb437d10e6e919c90e8415364be2d23
Jan Aleksandrov <jan.aleksandrov@gmail.com>
2019-07-21 11:33:49 +0200

    Added RTSP camera, tested added notebook

--- test.py
+++
@@ -1,4 +0,0 @@
-from jetcam.rtsp_camera import RTSPCamera
-camera = RTSPCamera(width=224, height=224, capture_width=640, capture_height=480,capture_source='rtsp://10.42.0.161:5540/ch0')
-#from jetcam.usb_camera import USBCamera
-#camera = USBCamera(width=224, height=224, capture_width=640, capture_height=480,capture_device=0)
